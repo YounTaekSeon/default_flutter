d825479fb65edefc7914743a1b046ce469255d02
YounTaekSeon <youneciohc@gmail.com>
2023-09-07 09:57:59 +0900

    screen 기본 틀 작성

---
+++ lib/widgets/screens/drags_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DragsScreen extends StatelessWidget {
+  const DragsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
