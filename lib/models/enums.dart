5329ebb96ec441d920607ed19a7f17cb0b979b31
YounTaekSeon <youneciohc@gmail.com>
2023-09-20 15:28:51 +0900

    enums 내부 skillstack(shotcut) 수정

--- lib/models/enums.dart
+++ lib/models/enums.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:youneciohc/widgets/screens/buttons_screen.dart';
 import 'package:youneciohc/widgets/screens/creates_screen.dart';
-import 'package:youneciohc/widgets/screens/drags_screen.dart';
 import 'package:youneciohc/widgets/screens/layouts_screen.dart';
-import 'package:youneciohc/widgets/screens/rotates_screen.dart';
+import 'package:youneciohc/widgets/screens/gesture_screen.dart';
+import 'package:youneciohc/widgets/screens/list_screen.dart';
 
 enum SkillStacks {
   LAYOUTS("레이아웃", LayoutsScreen()),
   BUTTONS("버튼", ButtonsScreen()),
-  ROTATES("회전", RotatesScreen()),
-  DRAGS("드래그",DragsScreen()),
+  ROTATES("제스처", GestureScreen()),
   CREATES("생성",CreatesScreen()),
+  List("리스트(정렬)",ListScreen()),
   ;
 
   const SkillStacks(this.name, this.widget);
