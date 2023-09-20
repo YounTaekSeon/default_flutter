aeb6cccf908e69ed4fbce82f0e5b1c24d8175ca4
YounTaekSeon <youneciohc@gmail.com>
2023-09-20 15:25:20 +0900

    welcome screen에 list 추가

--- lib/widgets/screens/welcome_screen.dart
+++ lib/widgets/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youneciohc/models/enums.dart';
 import 'package:youneciohc/widgets/commons/deafult_body.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -11,9 +12,44 @@ class WelcomeScreen extends StatelessWidget {
 }
 
 class _Body extends DefaultBody {
-
   @override
   Widget buildBody(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Skill Stack"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [_ShortCutMenu()],
+        ),
+      ),
+    );
+  }
+}
+
+class _ShortCutMenu extends StatelessWidget {
+  static const int COLUMN_COUNT = 2;
+  final List<SkillStacks> skillStacks = SkillStacks.values;
+
+  _ShortCutMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        primary: false,
+        shrinkWrap: true,
+        itemCount: skillStacks.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: COLUMN_COUNT),
+        itemBuilder: (BuildContext context, int index) {
+          SkillStacks skillStack = skillStacks[index];
+          return InkWell(
+            splashColor: Colors.blue,
+            onTap: () {
+              Navigator.of(context).push((MaterialPageRoute(builder: (context) => skillStack.widget)));
+            },
+            child: Center(child: Text(skillStack.name, style: TextStyle(color: Colors.black, fontSize: 20),)),
+          );
+        });
   }
 }
