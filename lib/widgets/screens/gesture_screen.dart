8154493825a9088ab026f9563d8acb7c65d6f4c2
YounTaekSeon <youneciohc@gmail.com>
2023-09-20 15:27:44 +0900

    RotateScreen에서 GestureScreen으로 이름 변경

---
+++ lib/widgets/screens/gesture_screen.dart
@@ -0,0 +1,22 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:youneciohc/utils/app_util.dart';
+import 'package:youneciohc/widgets/commons/deafult_body.dart';
+import 'package:youneciohc/widgets/commons/ink_wrapper.dart';
+
+class GestureScreen extends StatefulWidget {
+  const GestureScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GestureScreen> createState() => _GestureScreenState();
+}
+
+class _GestureScreenState extends State<GestureScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
